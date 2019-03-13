dc1afb6efc27ea93282d41b36d84ff27f18f5e9d
mingzhi.xie <xie6032mail@163.com>
2019-03-13 18:52:21 +0800

    add eureka server

---
+++ shop-cloud/server-center/src/main/java/cn/miven/cloud/center/CenterApplication.java
@@ -0,0 +1,13 @@
+package cn.miven.cloud.center;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class CenterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CenterApplication.class, args);
+    }
+}
