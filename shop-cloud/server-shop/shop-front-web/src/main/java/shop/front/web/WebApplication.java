5d33daece519d2c543282601b1211531c9e43b6f
mingzhi.xie <xie6032mail@163.com>
2019-04-03 21:06:29 +0800

    Init shop-front-web project

---
+++ shop-cloud/server-shop/shop-front-web/src/main/java/shop/front/web/WebApplication.java
@@ -0,0 +1,21 @@
+package shop.front.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @author mingzhi.xie
+ * @date 2019/4/3
+ */
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+public class WebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class, args);
+    }
+}
