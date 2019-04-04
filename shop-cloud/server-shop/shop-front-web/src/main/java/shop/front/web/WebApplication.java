ec4f5b27e26d10cfb34198420bbc89d43b5c77de
mingzhi.xie <xie6032mail@163.com>
2019-04-04 13:48:33 +0800

    Add openfeign

--- shop-cloud/server-shop/shop-front-web/src/main/java/shop/front/web/WebApplication.java
+++ shop-cloud/server-shop/shop-front-web/src/main/java/shop/front/web/WebApplication.java
@@ -3,6 +3,7 @@ package shop.front.web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
